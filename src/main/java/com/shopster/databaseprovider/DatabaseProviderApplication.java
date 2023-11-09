a2b3cb46d251d52d8ab4387c233cfc2d2a43c098
Bishesh99 <bisheshsingh.99@gmail.com>
2023-11-09 21:12:29 +0530

    Main file was left

--- src/main/java/com/shopster/databaseprovider/DatabaseProviderApplication.java
+++ src/main/java/com/shopster/databaseprovider/DatabaseProviderApplication.java
@@ -2,12 +2,9 @@ package com.shopster.databaseprovider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class DatabaseProviderApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DatabaseProviderApplication.class, args);
     }
-
 }
